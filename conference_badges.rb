004448b4b5ecc4e191388b22d73ee930848e3501
Brad Schrag <brad.schrag@gmail.com>
2018-11-04 17:08:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  messages = []
+  array.each do |name|
+    messages << badge_maker(name)
+  messages
+end
+
+def assign_rooms(array)
+  room_number = 1
+  assignments = []
+  array.each do |name|
+    break if room_number > 7
+    assignments << "Hello, #{name}! You'll be assigned to room #{room_number}"
